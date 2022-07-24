d5c3c7eab94d63cd892d9e5c3f23a218d8cf16e6
kadir <kdrokumus35@gmail.com>
2022-07-24 23:10:02 +0300

    negativeTest

--- src/test/java/tests/NegativeLoginTest.java
+++ src/test/java/tests/NegativeLoginTest.java
@@ -11,22 +11,53 @@ public class NegativeLoginTest {
     BrcPage brcPage=new BrcPage();
 
     @Test
-    public void yanlisSifre(){
-
-    // Bir test method olustur positiveLoginTest()
-
-
+    public void yanlisSifre() throws InterruptedException {
     //       https://www.bluerentalcars.com/ adresine git
         Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
     //      login butonuna bas
          brcPage.ilkLogin.click();
-    //  test data user email: customer@bluerentalcars.com ,
+    //  test data valid email
         brcPage.eMailAdress.sendKeys(ConfigReader.getProperty("brcValidEmail"));
-    //  test data password : 54321
+    //  test data wrong password
         brcPage.password.sendKeys(ConfigReader.getProperty("brcWrongPassword"));
     //  login butonuna tiklayin
         brcPage.ikinciLogin.click();
     //  Degerleri girilemedigini sayfaya basarili sekilde girilemedigini test et
+        Assert.assertTrue(brcPage.ikinciLogin.isDisplayed());
+
+        Driver.closeDriver();
+    }
+    @Test
+    public void yanlisKullanici() throws InterruptedException {
+
+        //       https://www.bluerentalcars.com/ adresine git
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+        //      login butonuna bas
+        brcPage.ilkLogin.click();
+        //  test data wrong email
+        brcPage.eMailAdress.sendKeys(ConfigReader.getProperty("brcWrongdEmail"));
+        //  test data wrong password
+        brcPage.password.sendKeys(ConfigReader.getProperty("brcValidPassword"));
+        //  login butonuna tiklayin
+        brcPage.ikinciLogin.click();
+        //  Degerleri girilemedigini sayfaya basarili sekilde girilemedigini test et
+        Assert.assertTrue(brcPage.ikinciLogin.isDisplayed());
+        Driver.closeDriver();
+
+    }
+    @Test
+    public void yanlisSifreVeKullanici(){
+        //       https://www.bluerentalcars.com/ adresine git
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+        //      login butonuna bas
+        brcPage.ilkLogin.click();
+        //  test data wrong email
+        brcPage.eMailAdress.sendKeys(ConfigReader.getProperty("brcWrongdEmail"));
+        //  test data wrong password
+        brcPage.password.sendKeys(ConfigReader.getProperty("brcValidPassword"));
+        //  login butonuna tiklayin
+        brcPage.ikinciLogin.click();
+        //  Degerleri girilemedigini sayfaya basarili sekilde girilemedigini test et
         Assert.assertTrue(brcPage.ikinciLogin.isDisplayed());
         Driver.closeDriver();
     }
